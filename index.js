07993870b18abfe2d19fde0f364fc5e1538379ee
shennan <jackotisbarker@gmail.com>
2014-06-18 11:21:39 +0100

    final fix for singleton issue

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-module.exports = function(imgs){
-
-	return new Kineograph(imgs);
-
-};
+module.exports = Kineograph;
 
 /**
 *		Initialize a new `Kineograph`.
@@ -14,258 +10,236 @@ function Kineograph(imgs) {
 
 	if(!document || !imgs) return;
 
-	var el = mixin( document.createElement('div') );
+	var _images = {};
+	var _animations = [];
+	var _animating = undefined;
+	var _showing = undefined;
+	var _unloop = false;
+	var _unloop_callback = undefined;
+	var _timeouts = [];
+	var _finish = false;
+	var _fps = 25;
 
-	el.className = 'kineograph';
+	var self = document.createElement('div');
+
+	self.className = 'kineograph';
 
 	if(imgs instanceof Array)
 		imgs = {'_default':imgs};
 
 	for(var i in imgs){
 		for(var j in imgs[i]){
-			image.apply(el, [imgs[i][j], i]);
+			image(imgs[i][j], i);
 		}
 	}
 
-  return el;
+	/**
+	*		Add an animation to the end of the cue and start playing if it isn't already.
+	*
+	*		@param {String} ani - the animation name to add
+	*		@param {Number} loop - the number of times the animation should play
+	*		@param {Function} callback - the optional method to call when finished
+	*		@return {Kineograph}
+	*		@api public
+	*/
 
-};
+	self.play = function(ani, loop, callback){
 
-/**
-*		Public variables
-*
-*		@api public
-*/
-
-Kineograph.prototype._images = {};
-Kineograph.prototype._animations = [];
-Kineograph.prototype._animating = undefined;
-Kineograph.prototype._unloop = false;
-Kineograph.prototype._unloop_callback = undefined;
-Kineograph.prototype._timeouts = [];
-Kineograph.prototype._finish = false;
-Kineograph.prototype._fps = 25;
-
-/**
-*		Add an animation to the end of the cue and start playing if it isn't already.
-*
-*		@param {String} ani - the animation name to add
-*		@param {Number} loop - the number of times the animation should play
-*		@param {Function} callback - the optional method to call when finished
-*		@return {Kineograph}
-*		@api public
-*/
-
-Kineograph.prototype.play = function(ani, loop, callback){
-
-	add.apply(this, [ani, loop, callback]);
-	
-	if(!this._animating) this.next();
-
-	return this;
-	
-}
-
-/**
-*		Begin animating the next animation in the cue
-*
-*		@return {Kineograph}
-*		@api public
-*/
-
-Kineograph.prototype.next = function(){
-
-	this._unloop = false;
-	this.stop();
-
-	if(!this._animations.length){ this._animating = false; return; }
-
-	this._animating = this._animations.shift();
-	
-	play.apply(this, [this._animating.ani, this._animating.loop, this._animating.callback]);
-
-	return this;
-
-}
-
-/**
-*		Unloop any currently looping animations and allow the sequence to move onwards.
-*
-*		@param {Function} callback - callback which is called when the loop has finished gracefully
-*		@return {Kineograph}
-*		@api public
-*/
-
-Kineograph.prototype.unloop = function(callback){
-
-	this._unloop = true;
-	this._unloop_callback = callback;
-
-	return this;
-
-}
-
-/**
-*		Stop any currently playing animations, without prejudice.
-*
-*		@return {Kineograph}
-*		@api public
-*/
-
-Kineograph.prototype.stop = function(){
-
-	while(this._timeouts.length){
+		add(ani, loop, callback)
 		
-		clearTimeout(this._timeouts.shift());
-	
+		if(!_animating) self.next();
+
+		return self;
+		
 	}
 
-	this._animating = undefined;
+	/**
+	*		Begin animating the next animation in the cue
+	*
+	*		@return {Kineograph}
+	*		@api public
+	*/
 
-	return this;
+	self.next = function(){
 
-}
+		_unloop = false;
+		self.stop();
 
-/**
-*		Change the fps.
-*
-*		@return {Kineograph}
-*		@api public
-*/
+		if(!_animations.length){ _animating = false; return; }
 
-Kineograph.prototype.fps = function(val){
-
-	if(typeof val === 'number')
-		this._fps = val;
-
-	return this;
-
-}
-
-/**
-*		Mixin the Kineograph properties.
-*
-*		@param {HTMLElement} el
-*		@return {HTMLElement}
-*		@api private
-*/
-
-function mixin(el) {
-  
-  for (var key in Kineograph.prototype){
-    el[key] = Kineograph.prototype[key];
-  }
-
-  return el;
-
-}
-
-/**
-*		Add a frame to the animation
-*
-*		@param {String} path - the relative path to the image
-*		@param {String} key - an animtion name for the image
-*		@api private
-*/
-
-function image(path, key){
-
-	var img = this.appendChild( document.createElement( 'img' ) );
-	img.src = path;
-	img.style.visibility = 'hidden';
-
-	if(this._images[key])
-		this._images[key].push(img);
-	else
-		this._images[key] = [img];
-
-}
-
-/**
-*		Add an animation to the cue
-*
-*		@param {String} ani - the animation name to add
-*		@param {Number} loop - the number of times the animation should play
-*		@param {Function} callback - the optional method to call when finished
-*		@api private
-*/
-
-function add(ani, loop, callback){
-
-	ani = typeof ani === 'string' ? ani : '_default';
-	loop = typeof loop === 'number' ? parseInt(loop) : 1;
-
-	if(!this._images[ani])
-		throw new Error(ani + ' does not exist');
-
-	this._animations.push({ani:ani, loop:loop, callback:callback});
-
-}
-
-/**
-*		Play a sequence of images from start to end
-*
-*		@param {String} ani - the animation name to play
-*		@param {Number} loop - the number of times the animation should play
-*		@param {Function} callback - the optional method to call when finished
-*		@api private
-*/
-
-function play(ani, loop, callback){
-
-	var indefinate = !loop;
-	
-	loop--;
-
-	var self = this;
-
-	for(var i = 0; i < this._images[ani].length; i++){
+		_animating = _animations.shift();
 		
-		var timeout = setTimeout(function(img, last){
+		play(_animating.ani, _animating.loop, _animating.callback)
+
+		return self;
+
+	}
+
+	/**
+	*		Unloop any currently looping animations and allow the sequence to move onwards.
+	*
+	*		@param {Function} callback - callback which is called when the loop has finished gracefully
+	*		@return {Kineograph}
+	*		@api public
+	*/
+
+	self.unloop = function(callback){
+
+		_unloop = true;
+		_unloop_callback = callback;
+
+		return self;
+
+	}
+
+	/**
+	*		Stop any currently playing animations, without prejudice.
+	*
+	*		@return {Kineograph}
+	*		@api public
+	*/
+
+	self.stop = function(){
+
+		while(_timeouts.length){
 			
-			return function(){
+			clearTimeout(_timeouts.shift());
+		
+		}
 
-				show(img);
+		_animating = undefined;
 
-				if(last){
+		return self;
 
-					if(indefinate && !self._unloop){
-						play.apply(self, [ani, loop, callback]);
-					}else{
-						if(!loop || self._unloop){
-							if(callback)
-								callback();
-							if(self._unloop_callback)
-								self._unloop_callback();
-							self.next();
+	}
+
+	/**
+	*		Change the fps.
+	*
+	*		@return {Kineograph}
+	*		@api public
+	*/
+
+	self.fps = function(val){
+
+		if(typeof val === 'number')
+			_fps = val;
+
+		return self;
+
+	}
+
+	/**
+	*		Add a frame to the animation
+	*
+	*		@param {String} path - the relative path to the image
+	*		@param {String} key - an animtion name for the image
+	*		@api private
+	*/
+
+	function image(path, key){
+
+		var img = self.appendChild( document.createElement( 'img' ) );
+		img.src = path;
+		img.style.visibility = 'hidden';
+
+		console.log(_images);
+
+		if(_images[key])
+			_images[key].push(img);
+		else
+			_images[key] = [img];
+
+	}
+
+	/**
+	*		Add an animation to the cue
+	*
+	*		@param {String} ani - the animation name to add
+	*		@param {Number} loop - the number of times the animation should play
+	*		@param {Function} callback - the optional method to call when finished
+	*		@api private
+	*/
+
+	function add(ani, loop, callback){
+
+		ani = typeof ani === 'string' ? ani : '_default';
+		loop = typeof loop === 'number' ? parseInt(loop) : 1;
+
+		if(!_images[ani])
+			throw new Error(ani + ' does not exist');
+
+		_animations.push({ani:ani, loop:loop, callback:callback});
+
+	}
+
+	/**
+	*		Play a sequence of images from start to end
+	*
+	*		@param {String} ani - the animation name to play
+	*		@param {Number} loop - the number of times the animation should play
+	*		@param {Function} callback - the optional method to call when finished
+	*		@api private
+	*/
+
+	function play(ani, loop, callback){
+
+		var indefinate = !loop;
+		
+		loop--;
+
+		for(var i = 0; i < _images[ani].length; i++){
+			
+			var timeout = setTimeout(function(img, last){
+				
+				return function(){
+
+					show(img);
+
+					if(last){
+
+						if(indefinate && !_unloop){
+							play(ani, loop, callback);
 						}else{
-							play.apply(self, [ani, loop, callback]);
+							if(!loop || _unloop){
+								if(callback)
+									callback();
+								if(_unloop_callback)
+									_unloop_callback();
+								self.next();
+							}else{
+								play(ani, loop, callback);
+							}
 						}
 					}
 				}
-			}
 
-		}(this._images[ani][i], !(i < this._images[ani].length - 1)), 1000 / this._fps * i);
+			}(_images[ani][i], !(i < _images[ani].length - 1)), 1000 / _fps * i);
 
-		this._timeouts.push(timeout);
+			_timeouts.push(timeout);
+
+		}
+	}
+
+	/**
+	*		Show a particular image
+	*
+	*		@param {HTMLElement} img - the image element to show
+	*		@api private
+	*/
+
+	function show(img){
+
+		img.style.visibility = 'visible';
+
+		if(_showing)
+			_showing.style.visibility = 'hidden';
+
+		_showing = img;
 
 	}
-}
 
-/**
-*		Show a particular image
-*
-*		@param {HTMLElement} img - the image element to show
-*		@api private
-*/
+  return self;
 
-function show(img){
+};
 
-	img.style.visibility = 'visible';
-
-	if(this._showing)
-		this._showing.style.visibility = 'hidden';
-
-	this._showing = img;
-
-}
